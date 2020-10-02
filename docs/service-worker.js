15af6cc2ff186cc77fe2c4f6b8c0f03ed50fd8b8
Panagiotis Kyriazopoulos <pkyriazo@gmx.com>
2020-10-02 12:24:02 +0300

    http -> https

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://pkyriazo.github.io/coursera_search_react/precache-manifest.bf971c9b126dd5b9ba572e53ec1a0301.js"
+  "https://pkyriazo.github.io/coursera_search_react/precache-manifest.3b52925ca353389b5d7cb5f4dcec126f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://pkyriazo.github.io/coursera_search_react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://pkyriazo.github.io/coursera_search_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
