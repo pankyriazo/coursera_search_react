13d60bb1c641c0befe729692257df1af49ec28d6
Panagiotis Kyriazopoulos <pkyriazo@gmx.com>
2020-10-02 12:30:40 +0300

    Updates

---
+++ precache-manifest.3b52925ca353389b5d7cb5f4dcec126f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f82d71ca6a8fead27199307e08672ee",
+    "url": "https://pkyriazo.github.io/coursera_search_react/index.html"
+  },
+  {
+    "revision": "875e7ab7fc9d5b8efc54",
+    "url": "https://pkyriazo.github.io/coursera_search_react/static/css/main.a65ecdf3.chunk.css"
+  },
+  {
+    "revision": "67f1fe4bab55e2c49929",
+    "url": "https://pkyriazo.github.io/coursera_search_react/static/js/2.bcd57354.chunk.js"
+  },
+  {
+    "revision": "799983770b9b37b40b0db4b53b39db52",
+    "url": "https://pkyriazo.github.io/coursera_search_react/static/js/2.bcd57354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875e7ab7fc9d5b8efc54",
+    "url": "https://pkyriazo.github.io/coursera_search_react/static/js/main.2b95a541.chunk.js"
+  },
+  {
+    "revision": "053ffc93ce4cc6e537e8",
+    "url": "https://pkyriazo.github.io/coursera_search_react/static/js/runtime-main.2494889e.js"
+  }
+]);
